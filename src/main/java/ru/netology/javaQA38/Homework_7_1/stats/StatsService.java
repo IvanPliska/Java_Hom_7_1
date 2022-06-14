c52872e41ae79e8ecf7d5fc39172cdb58dd32473
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-06-14 16:34:26 +0300

    Homework_7_1

---
+++ src/main/java/ru/netology/javaQA38/Homework_7_1/stats/StatsService.java
@@ -0,0 +1,78 @@
+package ru.netology.javaQA38.Homework_7_1.stats;
+
+public class StatsService {
+
+    // 1.Суммa всех продаж
+
+    public int sumSales(long[] sales) {
+        int summa = 0;
+        for (long sum : sales) {
+            summa += sum;
+        }
+        return summa;
+
+    }
+
+    // 2. Средняя сумма продаж в месяц
+    public int averageSumSales(long[] sales) {
+        int averade = sumSales(sales);
+        return averade / 12;
+    }
+
+    // 3. Номер месяца в которром был пик продаж
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+
+    // 4. Номер месяца в котором был мин продаж
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    // 5. Кол-во месяцев, в которых продажи были ниже среднего
+
+    public int countMonthDownAverageSales(long[] sales) {
+        int downMonth = 0;
+        int averageSumm = averageSumSales(sales);
+        for (long sale : sales) {
+            if (sale < averageSumm) {
+                downMonth++;
+            }
+
+        }
+        return downMonth;
+    }
+
+    // 6. Кол-во месяцев, в которых продажи были выше среднего
+
+    public int countMonthUpperAverageSales(long[] sales) {
+        int upperMonth = 0;
+        int averageSumm = averageSumSales(sales);
+        for (long sale : sales) {
+            if (sale > averageSumm) {
+                upperMonth++;
+            }
+
+        }
+        return upperMonth;
+    }
+}
---
+++ src/test/java/ru/netology/javaQA38/Homework_7_1/stats/StatsServiceTest.java
@@ -0,0 +1,87 @@
+package ru.netology.javaQA38.Homework_7_1.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test // 1. Тест суммы всех продаж
+
+    public void shouldSummSalesOfMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int summa = 180;
+        int actualSum = service.sumSales(sales);
+        Assertions.assertEquals(summa, actualSum);
+
+    }
+
+    @Test // 2. Тест средней суммы всех продаж
+
+    public void shouldFindAverageSummSalesOfMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int averageSumma = 15;
+        int actualAverageSumm = service.averageSumSales(sales);
+        Assertions.assertEquals(averageSumma, actualAverageSumm);
+    }
+
+
+    @Test // 3. Тест номера месяца в котором был пик продаж
+
+    public void shouldFindMaxSalesMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int month = 8;
+        int actualyMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(month, actualyMonth);
+    }
+
+    @Test // 4. Тест номер месяца в котором был мин продаж
+
+    public void shouldFindMinSalesMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int month = 9;
+        int actualyMonth = service.minSales(sales);
+
+        Assertions.assertEquals(month, actualyMonth);
+    }
+
+    @Test // 5. Тест кол-во месяцев, в которых продажи были ниже среднего
+
+    public void shouldFindMonthMinAverageSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int monthCount = 5;
+        int actualMonthCount;
+        actualMonthCount = service.countMonthDownAverageSales(sales);
+
+        Assertions.assertEquals(monthCount, actualMonthCount);
+
+    }
+
+    @Test // 6. Тест кол-во месяцев, в которых продажи были выше среднего
+
+    public void shouldFindMonthMaxAverageSales() {
+
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int monthCount = 5;
+        int actualMonthCount = service.countMonthUpperAverageSales(sales);
+
+        Assertions.assertEquals(monthCount, actualMonthCount);
+    }
+}
